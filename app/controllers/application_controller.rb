e9b539dba2f6856a7a03933d2adc3bfb94be5919
iyuuya <vim.ito@1pac.jp>
2016-09-09 10:35:42 +0900

    error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   include Banken
   protect_from_forgery with: :exception
+
+  rescue_from Banken::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    loyalty_name = exception.loyalty.class.to_s.underscore
+
+    flash[:error] = t "#{loyalty_name}.#{exception.query}", scope: 'banken', default: :default
+    redirect_to(request.referrer || root_path)
+  end
 end
